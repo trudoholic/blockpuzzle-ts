0ce9149e3cc4565e288f8aef3d8c9831a954a1b7
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-13 17:22:51 +0300

    fixes 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GameClient from "./components/GameClient";
 
 function App() {
   return (
-    <div className="h-[100%] flex justify-center">
+    <div className="h-[100vh] flex justify-center bg-zinc-800 text-zinc-500">
       <GameClient />
     </div>
   )
