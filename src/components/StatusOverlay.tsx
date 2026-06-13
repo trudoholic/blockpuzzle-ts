2ef0f7b309d1f95f69f4153c69dba7e08cc3032a
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-06-13 16:55:50 +0300

    canvas

---
+++ src/components/StatusOverlay.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { type State } from "../store";
+import { useGameStore } from "./GameClient";
+
+const statusText: Partial<Record<State["status"], string>> = {
+  idle: "Press enter or click/tap the grid to start",
+  loading: "Loading...",
+  gameover: "Game Over. Press enter to reset",
+  started: ""
+};
+
+const StatusOverlay = () => {
+  const status = useGameStore((state) => {
+
+    return statusText[state.status] ?? state.status;
+  });
+
+  return <h3 className="uppercase text-center z-40 mx-8">{status}</h3>;
+};
+
+export default React.memo(StatusOverlay);
